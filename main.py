399a77c098c24c6b3f71ef241888dcec29525877
GHCMaxim <maxim080503@gmail.com>
2023-06-08 22:18:14 +0700

    feat(component): added new tables

--- main.py
+++ main.py
@@ -22,19 +22,55 @@ def main():
         """)
 
     if exists == 0:
+        print("It seems like you don't have a database yet. Creating tables....")
         cursor.execute("""
             CREATE TABLE Students(
                 StudentID int not null,
                 StudentName varchar(255) not null,
                 DateOfBirth date not null,
-                PRIMARY KEY (StudentID)
-                FOREIGN KEY (StudentID) REFERENCES Students(StudentID) 
+                PRIMARY KEY (StudentID) 
             )
             """)
-    if exists == 1:
         cursor.execute("""
-            DROP TABLE Students
+            CREATE TABLE Courses(
+                CourseID int not null,
+                CourseName varchar(255) not null,
+                PRIMARY KEY (CourseID)
+                FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID)
+            )
             """)
+        cursor.execute("""
+            CREATE TABLE Grades(
+                StudentID int not null,
+                CourseID int not null,
+                Grade int not null,
+                PRIMARY KEY (StudentID, CourseID),
+                FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+                FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+            )
+            """)
+        cursor.execute("""
+            CREATE TABLE Teachers(
+                TeacherID int not null,
+                TeacherName varchar(255) not null,
+                PRIMARY KEY (TeacherID)
+            )
+            """)
+        cursor.execute("""
+            CREATE TABLE Attendance(
+                StudentID int not null,
+                CourseID int not null,
+                AttendanceDate date not null,
+                AttendanceStatus bit not null,
+                PRIMARY KEY (StudentID, CourseID),
+                FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+                FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+            )
+            """)
+        cursor.commit()
+
+    if exists == 1:
+        print("Loading...")
 
 if __name__ == "__main__":
     try:
