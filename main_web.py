ebca7888f057b3da0a1d8eba0f0262b6d705e555
GHCMaxim <maxim080503@gmail.com>
2023-07-13 00:29:01 +0700

    feat(web): setup web main file

--- main_web.py
+++ main_web.py
@@ -1,40 +1,52 @@
-import sys
-
-from option import Ok, Result
-from frontend.helper_tui import *
-from database.mssql import cursor, conn
-from frontend.tui import *
 from flask import Flask, render_template
+from flask_restful import Api
+
+from models_WEB import Attendance, Course, Grade, Student, Teacher
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/attendance')
+
+@app.route("/attendance")
 def attendance():
-    return render_template('attendance.html')
+    return render_template("attendance.html")
 
-@app.route('/course')
+
+@app.route("/course")
 def course():
-    return render_template('course.html')
+    return render_template("course.html")
 
-@app.route('/student')
+
+@app.route("/student")
 def student():
-    return render_template('student.html')
+    return render_template("student.html")
 
-@app.route('/teacher')
+
+@app.route("/teacher")
 def teacher():
-    return render_template('teacher.html')
+    return render_template("teacher.html")
 
-@app.route('/index')
+
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/grade')
+
+@app.route("/grade")
 def grade():
-    return render_template('grade.html')
+    return render_template("grade.html")
 
-@app.route('/home')
+
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
+api.add_resource(Attendance, "/api/attendance")
+api.add_resource(Course, "/api/course")
+api.add_resource(Student, "/api/student")
+api.add_resource(Teacher, "/api/teacher")
+api.add_resource(Grade, "/api/grade")
 
+if __name__ == "__main__":
+    app.run()
