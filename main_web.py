8032596bc8dc2d12e46a64a321a1d943be015df7
GHCMaxim <maxim080503@gmail.com>
2023-07-14 02:15:32 +0700

    refactor(main_web.py): import all from models_web

--- main_web.py
+++ main_web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_restful import Api
 
-from models_WEB import Attendance, Course, Grade, Student, Teacher
+from models_WEB import *
 
 app = Flask(__name__)
 api = Api(app)
