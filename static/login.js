698a0b630071a190c22b922d1a25d637c0a190f2
GHCMaxim <maxim080503@gmail.com>
2023-07-15 16:06:18 +0700

    style: lint

--- static/login.js
+++ static/login.js
@@ -1,35 +1,37 @@
 const listAccount = [
     {
         email: "hiep@gmail.com",
-        password: "123456"
+        password: "123456",
     },
     {
         email: "khanh@gmail.com",
-        password: "123456"
+        password: "123456",
     },
     {
         email: "ha@gmail.com",
-        password: "123456"
+        password: "123456",
+    },
+];
+
+let isLogin = !!localStorage.getItem("token");
+
+function CheckLogin() {
+    if (isLogin) {
+        window.location.href = "home.html";
     }
-]
-
-let isLogin = !!localStorage.getItem("token")
-
-function CheckLogin(){
-    if (isLogin){
-        window.location.href = "home.html"
-    } 
 }
 
-function Login(){
+function Login() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    let checkLogin = listAccount.some(value => value.email == email && value.password == password)
-    if (checkLogin){
-        localStorage.setItem("token", email)
-        isLogin = true
-        CheckLogin()
+    let checkLogin = listAccount.some(
+        (value) => value.email == email && value.password == password,
+    );
+    if (checkLogin) {
+        localStorage.setItem("token", email);
+        isLogin = true;
+        CheckLogin();
     } else {
-        alert("Wrong email or password.")
+        alert("Wrong email or password.");
     }
 }
