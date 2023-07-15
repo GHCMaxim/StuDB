e9d34689918748f3e20073b9a5a2b0b6fa838ff1
GHCMaxim <maxim080503@gmail.com>
2023-07-15 15:54:17 +0700

    feat(UserAPIs): 3 seperate classes for Login/Out/Register

--- models_WEB/__init__.py
+++ models_WEB/__init__.py
@@ -3,8 +3,18 @@ from .course import CourseAPI
 from .grade import GradeAPI
 from .student import StudentAPI
 from .teacher import TeacherAPI
-from .user import UserAPI
+from .user import LoginAPI, LogoutAPI, RegisterAPI, ValidateSessionAPI
 
 # from .login import Login
 
-__all__ = ["AttendanceAPI", "CourseAPI", "GradeAPI", "StudentAPI", "TeacherAPI", "UserAPI"]
+__all__ = [
+    "AttendanceAPI",
+    "CourseAPI",
+    "GradeAPI",
+    "StudentAPI",
+    "TeacherAPI",
+    "LoginAPI",
+    "LogoutAPI",
+    "RegisterAPI",
+    "ValidateSessionAPI",
+]
--- models_WEB/user.py
+++ models_WEB/user.py
@@ -1,47 +1,122 @@
 from __future__ import annotations
 
+import uuid
 
 from flask_restful import Resource, request
 
 from database.login import global_var
 from database.mssql import conn, cursor
-from frontend.helper_web.MESSAGE import CREATE_USER_MSG, MISSING_ARGS_MSG
+from frontend.helper_web.MESSAGE import CREATE_USER_MSG, INVALID_ROLE, INVALID_SESSION, MISSING_ARGS_MSG
 from frontend.helper_web.validate_args import validate_args
+from frontend.helper_web.is_first_user import is_first_user
 
-
-class UserAPI(Resource):
+class LoginAPI(Resource):
     def post(self):
         """User login"""
-        validate_success, message_body, missing_args = validate_args(
+        validate_success, message, missing_args = validate_args(
             request.get_json(silent=True), tuple(["username", "password"])
         )
         if not validate_success:
             return {"message": MISSING_ARGS_MSG(missing_args), "data": {}}, 400
         username, password = message["username"], message["password"]
 
-        cursor.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
+        cursor.execute(f"SELECT * FROM Users WHERE Username='{username}' AND Password='{password}'")
         db_result = cursor.fetchone()
         if db_result is None:
             return {"message": "Wrong username or password"}, 400
 
-        cursor.execute(f"INSERT INTO users (username, password) VALUES ('{username}', '{password}')")
-        conn.commit()
-        return jsonify({"message": CREATE_USER_MSG("created", username)}), 201
+        global_var["current_user"] = db_result[0]
+        global_var["current_user_role"] = db_result[2].capitalize()
+        global_var["session_key"] = str(uuid.uuid4())
 
-    def put(self):
-        """User register"""
-        validate_success, message_body, missing_args = validate_args(
-            request.get_json(silent=True), tuple(["username", "password"])
+        return (
+            {"message": CREATE_USER_MSG("logged in", username), "data": {"session_key": global_var["session_key"]}},
+            200,
+        )
+
+
+class LogoutAPI(Resource):
+    def post(self):
+        """User logout"""
+        validate_success, message, missing_args = validate_args(
+            request.get_json(silent=True), tuple(["session_key"])
         )
         if not validate_success:
-        username, password = message_body["username"], message_body["password"]
             return {"message": MISSING_ARGS_MSG(missing_args)}, 400
+        session_key = message["session_key"]
 
-        cursor.execute(f"SELECT * FROM users WHERE Username='{username}' AND Password='{password}'")
+        if session_key != global_var["session_key"]:
+            return {"message": INVALID_SESSION}, 400
+
+        global_var["current_user"] = ""
+        global_var["current_user_role"] = ""
+        global_var["session_key"] = ""
+
+        return {"message": "Logged out"}, 200
+
+
+class RegisterAPI(Resource):
+    def get(self):
+        if is_first_user():
+            return {"message": True}, 200
+        else:
+            return {"message": False}, 200
+
+    def post(self):
+        """User register"""
+        validate_success, message_body, missing_args = validate_args(
+            request.get_json(silent=True), tuple(["username", "password", "session_key", "role"])
+        )
+        if not validate_success:
+            return {"message": MISSING_ARGS_MSG(missing_args)}, 400
+        username, password, session_key, role = (
+            message_body["username"],
+            message_body["password"],
+            message_body["session_key"],
+            message_body["role"].capitalize(),
+        )
+
+        role = "Student" if role == "" else role
+        if username == "" or password == "":
+            return {"message": "Username or password cannot be empty"}, 400
+
+        if is_first_user():
+            cursor.execute(f"INSERT INTO Users VALUES ('{username}', '{password}', 'Admin')")
+            conn.commit()
+            return {"message": CREATE_USER_MSG("created", username)}, 200
+        else:
+            if session_key != global_var["session_key"]:
+                return {"message": INVALID_SESSION}, 400
+            if global_var["current_user_role"] != "Admin":
+                return {"message": INVALID_ROLE}, 400
+
+        cursor.execute(f"SELECT * FROM Users WHERE Username='{username}'")
         db_result = cursor.fetchone()
-        if db_result is None:
+        if db_result is not None:
             return {"message": CREATE_USER_MSG("already exists", username)}, 400
 
-        global_var["current_user"] = db_result[0]
-        global_var["current_user_role"] = db_result[3]
-        return jsonify({"message": CREATE_USER_MSG("logged in", username)}), 201
+        cursor.execute(f"INSERT INTO Users VALUES ('{username}', '{password}', '{role}')")
+        conn.commit()
+
+        return {"message": CREATE_USER_MSG("created", username)}, 200
+
+
+class ValidateSessionAPI(Resource):
+    def post(self):
+        """Validate session"""
+        validate_success, message_body, missing_args = validate_args(request.get_json(silent=True), tuple(["session_key"]))
+        if not validate_success:
+            return {"message": MISSING_ARGS_MSG(missing_args)}, 400
+        session_key = message_body["session_key"]
+
+        if session_key != global_var["session_key"]:
+            return {"message": INVALID_SESSION}, 400
+
+        return (
+                {
+                    "message": "Valid session",
+                    "data": {"role": global_var["current_user_role"], "username": global_var["current_user"]},
+                }
+            ,
+            200,
+        )
