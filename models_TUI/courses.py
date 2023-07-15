c4c47b457216d84f205dd23cf49c3299e2834c50
GHCMaxim <maxim080503@gmail.com>
2023-07-15 16:02:09 +0700

    refactor(models_tui/courses.py): remove CourseAPI and unused imports

--- models_TUI/courses.py
+++ models_TUI/courses.py
@@ -2,51 +2,14 @@ from __future__ import annotations
 
 import sys
 
-from flask import jsonify
-from flask_restful import Resource, request
 from option import Err, Ok, Result
 
-from database.mssql import conn, cursor
+from database.mssql import cursor
 
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
-
-
-class CoursesAPI(Resource):
-    def get(self):
-        cursor.execute("SELECT * FROM Courses")
-        result = cursor.fetchall()
-
-        # course_id
-        #   - course_name: course_name
-        #   - teacher_id: teacher_id
-        #   - credits: credits
-        # ...
-
-        return_data = {}
-        for course in result:
-            return_data[course[0]] = {"course_name": course[1], "teacher_id": course[2], "credits": course[3]}
-        return jsonify(result), 200
-
-    def post(self):
-        data = request.get_json()
-        course_id, course_name, teacher_id, credits = (
-            data["course_id"],
-            data["course_name"],
-            data["teacher_id"],
-            data["credits"],
-        )
-
-        # write to the database, no need for Course class
-        cursor.execute(
-            "INSERT INTO Courses (CourseID, CourseName, TeacherID, Credits) VALUES (%s, %s, %s, %s)",
-            (course_id, course_name, teacher_id, credits),
-        )
-        conn.commit()
-
-        return jsonify({"message": "Course added successfully"}), 201
 
 
 class Courses:
