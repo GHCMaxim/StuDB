cbfdaf2948ad3a6e68c3cf87d991787e517c2204
GHCMaxim <maxim080503@gmail.com>
2023-07-16 20:59:08 +0700

    refactor(templates.pug): splitting components

--- templates.pug/build.js
+++ templates.pug/build.js
@@ -11,8 +11,37 @@ const files = fs.readdirSync(sourcePath).filter((file) => {
     return file.match(/\.pug$/);
 });
 
+const htmlstring_to_html = (htmlstring) => {
+    const div = document.createElement("div");
+    div.innerHTML = htmlstring.trim();
+    return div.firstChild;
+};
+
 files.forEach((file) => {
-    const compiledFunction = pug.compileFile(sourcePath + file);
-    const html = compiledFunction();
-    fs.writeFileSync(outputPath + file.replace(/\.pug$/, ".html"), html);
+    const compiledFunction = pug.compileFile(
+        sourcePath + file,
+        (options = {
+            filters: {
+                table_type_filter: (text, options) => {
+                    text = pug.render(text);
+                    text = text.replace(/__table_type__/g, options.type);
+                    text = text.replace(
+                        /__Table_type__/g,
+                        options.type.charAt(0).toUpperCase() +
+                            options.type.slice(1),
+                    );
+                    text = text.replace(
+                        /__TABLE_TYPE__/g,
+                        options.type.toUpperCase(),
+                    );
+                    return text;
+                },
+            },
+        }),
+    );
+
+    fs.writeFileSync(
+        outputPath + file.replace(/\.pug$/, ".html"),
+        compiledFunction(),
+    );
 });
